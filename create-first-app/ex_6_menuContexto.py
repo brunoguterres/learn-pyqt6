cfbc80fc775723d2de73bd8df46cf64b5dda4c19
Bruno Guterres <brunoguterres26@gmail.com>
2023-04-29 16:47:00 -0300

    utilizando sinais para criar menu de contexto

--- create-first-app/ex_6_menuContexto.py
+++ create-first-app/ex_6_menuContexto.py
@@ -11,12 +11,15 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle('Janela de teste')
 
-    def contextMenuEvent(self, e):
+        self.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.on_context_menu)
+
+    def on_context_menu(self, pos):
         context = QMenu(self)
         context.addAction(QAction("test 1", self))
         context.addAction(QAction("test 2", self))
         context.addAction(QAction("test 3", self))
-        context.exec(e.globalPos())
+        context.exec(self.mapToGlobal(pos))
 
 
 app = QApplication(sys.argv)
