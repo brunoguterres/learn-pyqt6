2d41bc2eefc2573441aa4197e783e12bf336bfea
Bruno Guterres <brunoguterres26@gmail.com>
2023-04-29 14:11:03 -0300

    atualização do arquivo app

--- create-first-app/app.py
+++ create-first-app/app.py
@@ -1,19 +1,17 @@
-from PyQt6.QtWidgets import QApplication, QWidget
-
-# Only needed for access to command line arguments
 import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
-# You need one (and only one) QApplication instance per application.
-# Pass in sys.argv to allow command line arguments for your app.
-# If you know you won't use command line arguments QApplication([]) works too.
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+
 app = QApplication(sys.argv)
 
-# Create a Qt widget, which will be our window.
-window = QWidget()
-window.show()  # IMPORTANT!!!!! Windows are hidden by default.
+window = MainWindow()
+window.show()
 
-# Start the event loop.
 app.exec()
-
-# Your application won't reach here until you exit and the event
-# loop has stopped.
