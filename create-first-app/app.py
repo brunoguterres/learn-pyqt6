15bbaa4a0cd78d1856bf07bfea4912467bcc614d
Bruno Guterres <brunoguterres26@gmail.com>
2023-04-29 15:10:08 -0300

    slot para alteração da interface

--- create-first-app/app.py
+++ create-first-app/app.py
@@ -11,16 +11,16 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("My App")
 
         self.button = QPushButton("Press Me!")
-        self.button.setCheckable(True)
-        self.button.released.connect(self.the_button_was_toggled)
-        self.button.setChecked(self.button_is_checked)
+        self.button.released.connect(self.the_button_was_clicked)
 
         self.setCentralWidget(self.button)
 
-    def the_button_was_toggled(self):
-        self.button_is_checked =  self.button.isChecked()
-
-        print(self.button_is_checked)
+    def the_button_was_clicked(self):
+        self.button.setText("You already clicked me.")
+        self.button.setEnabled(False)
+        
+        # Also change the window title.
+        self.setWindowTitle("My Oneshot App")
 
 
 app = QApplication(sys.argv)
