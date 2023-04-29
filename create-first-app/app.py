4aed8f30853bea4be9d12e59d67105bc12791e1a
Bruno Guterres <brunoguterres26@gmail.com>
2023-04-29 14:21:54 -0300

    inserindo slot de check do status do botão

--- create-first-app/app.py
+++ create-first-app/app.py
@@ -11,12 +11,15 @@ class MainWindow(QMainWindow):
         button = QPushButton("Press Me!")
         button.setCheckable(True)
         button.clicked.connect(self.the_button_was_clicked)
+        button.clicked.connect(self.the_button_was_toggled)
 
-        # Set the central widget of the Window.
         self.setCentralWidget(button)
 
     def the_button_was_clicked(self):
         print("Clicked!")
+    
+    def the_button_was_toggled(self, checked):
+        print("Checked?", checked)
 
 
 app = QApplication(sys.argv)
