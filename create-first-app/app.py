af1151989a5cce7fd328a15d12347d5adf2ae6d6
Bruno Guterres <brunoguterres26@gmail.com>
2023-04-29 12:18:40 -0300

    excluindo linha

--- create-first-app/app.py
+++ create-first-app/app.py
@@ -15,6 +15,5 @@ window.show()  # IMPORTANT!!!!! Windows are hidden by default.
 # Start the event loop.
 app.exec()
 
-
 # Your application won't reach here until you exit and the event
 # loop has stopped.
